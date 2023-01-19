e19d4b7ca03a57120d2b4376f8a71946d787aaac
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-18 21:55:10 -0300

    feat: Ajusted the params in HabitDay.

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,15 +1,7 @@
-import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
-import { HabitDay } from "./HabitDay";
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
+import { HabitDay } from './HabitDay'
 
-const weekDays = [
-  'D',
-  'S',
-  'T',
-  'Q',
-  'Q',
-  'S',
-  'S',
-];
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesFromYearBeginning()
 
@@ -33,18 +25,26 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
+        {summaryDates.map(date => {
           return (
-            <HabitDay key={date.toString()} />
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
           )
         })}
 
-        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
-          return (
-            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
-          )
-        })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            )
+          })}
       </div>
     </div>
-  );
+  )
 }
