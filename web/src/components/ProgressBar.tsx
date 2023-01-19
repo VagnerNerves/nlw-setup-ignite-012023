9a45036282cb2de1c6844a6b01992f90234c05d3
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-18 21:25:55 -0300

    feat: Created ProgressBar.

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,12 @@
+export function ProgressBar() {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={75}
+        className="h-3 rounded-xl bg-violet-600 w-3/4"
+      />
+    </div>
+  )
+}
