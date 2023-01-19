1cae2a3ca11277a375a33824322a2d23c7a0ebf3
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-18 21:31:08 -0300

    feat: Ajusted Progress Bar with param progress.

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,11 +1,20 @@
-export function ProgressBar() {
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  const progressStyles = {
+    width: `${props.progress}%`
+  }
+
   return (
     <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
       <div
         role="progressbar"
         aria-label="Progresso de hábitos completados nesse dia"
-        aria-valuenow={75}
-        className="h-3 rounded-xl bg-violet-600 w-3/4"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={progressStyles}
       />
     </div>
   )
